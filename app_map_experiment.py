208cbe519fb19c8e2cf5d16bfecdb9376c5d1086
lucasvm2511 <lucasvanmiddendorp@gmail.com>
2022-01-08 14:02:02 +0100

    location

--- app_map_experiment.py
+++ app_map_experiment.py
@@ -17,11 +17,18 @@ app = dash.Dash(__name__)
 road_df = pd.read_csv('dataset.csv')
 names_df = pd.read_csv('names.csv')
 names_df.set_index('district_id', inplace=True)
+population_df = pd.read_csv('population_districts.csv')
+population_df.set_index('district',inplace=True)
 
 # Manipulate dataframe to get the data needed
 my_map = names_df.to_dict()
+my_map_population = population_df.to_dict()
 road_df['Local_Authority_(District)'].replace(my_map['district_name'],
                                               inplace=True)  # replace district id with district name
+road_df['Population']=road_df['Local_Authority_(District)']
+road_df['Population'].replace(my_map_population['population'],
+                                              inplace=True)  # replace district name with district population
+
 # Read in geojson
 hucs = gpd.read_file('geojson4.json')
 
