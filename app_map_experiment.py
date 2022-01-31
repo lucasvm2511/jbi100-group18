b0171f491ef242cf79744569573e5489a095370f
lucasvm2511 <lucasvanmiddendorp@gmail.com>
2022-01-31 14:10:39 +0100

    bug fixes en amount per 10000 toegevoegd
    ValueError: Invalid property specified for object of type plotly.graph_objs.layout.mapbox.Center: 'zoom'
    
    Did you mean "lon"?
    
    Valid properties:
    lat
    Sets the latitude of the center of the map (in degrees
    North).
    lon
    Sets the longitude of the center of the map (in degrees
    East).
    
    Did you mean "lon"?
    
    Bad property path:
    zoom

--- app_map_experiment.py
+++ app_map_experiment.py
@@ -59,6 +59,8 @@ from geojson_rewind import rewind
 hucs_rewound = rewind(hucs, rfc7946=False)
 severity = road_df.Casualty_Severity.unique()
 Weather_Conditions = road_df.Weather_Conditions.unique()
+junction_control = road_df.Junction_Control.unique()
+
 # ----------------------------------------------------------------------
 # App layout: Describes what the application look like; aka dash components (without data)
 app.layout = html.Div([
@@ -90,12 +92,10 @@ app.layout = html.Div([
               html.P("Pick junction control:"),
               dcc.Dropdown(id="junction_control",
                            options=[{'label': 'All', 'value': 'All'},
-                                    {'label': 'Not a junction or within 20 meters', 'value': 0},
-                                    {'label': 'Authorised person', 'value': 1},
-                                    {'label': 'Auto traffic signal', 'value': 2},
-                                    {'label': 'Stop sign', 'value': 3},
-                                    {'label': 'Give way or uncontrolled', 'value': 4},
-                                    {'label': 'Unknown', 'value': 9}],
+                                    {'label': 'Authorised person', 'value': junction_control[1]},
+                                    {'label': 'Auto traffic signal', 'value': junction_control[2]},
+                                    {'label': 'Stop sign', 'value': junction_control[3]},
+                                    {'label': 'Give way or uncontrolled', 'value': junction_control[4]}],
                            multi=False,
                            value='All',
                            style={}
@@ -105,7 +105,8 @@ app.layout = html.Div([
     html.Div(children=[
         html.Button('Show density heatmap', id='btn_2', n_clicks=0),
         html.Button('Show district heatmap', id='btn_1', n_clicks=0),
-        html.Button('Set colors', id='btn_3', n_clicks=0),
+        html.Button('Change colors', id='btn_3', n_clicks=0),
+        html.Button('Change to absolute/relative amount', id='btn_4', n_clicks=0),
         dcc.Graph(id="fig_map", figure={}, config={'scrollZoom': False}),
         html.Div(id='output_container', children=[]),
         html.Div(id='output_container2', children=[]),
@@ -122,10 +123,11 @@ app.layout = html.Div([
      Input(component_id="junction_control", component_property="value"),
      Input(component_id="btn_1", component_property='n_clicks'),
      Input(component_id="btn_2", component_property='n_clicks'),
-     Input(component_id="btn_3", component_property='n_clicks')]
+     Input(component_id="btn_3", component_property='n_clicks'),
+     Input(component_id="btn_4", component_property='n_clicks')]
 )
 # Argument in function refers to component_property in the Input(), here 3 arguments so three inputs in callback
-def update_graph(option_selected, option_selected2, option_selected3, n_clicks_b1, n_clicks_b2, n_clicks_b3):
+def update_graph(option_selected, option_selected2, option_selected3, n_clicks_b1, n_clicks_b2, n_clicks_b3,n_clicks_b4):
     if option_selected == 'All':
         filtered_df_casualty = road_df  # if all is selected, do not filter
     else:
@@ -142,25 +144,50 @@ def update_graph(option_selected, option_selected2, option_selected3, n_clicks_b
     filtered_df_junction['Accidents_amount'] = filtered_df_junction['Accident_Index']
     filtered_group_df = filtered_df_junction.groupby('Local_Authority_(District)').count()[
         'Accidents_amount'].to_frame().reset_index()
-
+    merged = pd.merge(left=filtered_group_df, right=population_df, left_on='Local_Authority_(District)',
+                      right_on='district')
+    merged['per10000'] = merged['Accidents_amount'] * 10000 / merged['population']
     changed_id = [p['prop_id'] for p in callback_context.triggered][0]
     colorscale_list = [px.colors.sequential.tempo, px.colors.sequential.Reds, px.colors.sequential.matter, px.colors.sequential.Cividis]
 
-    if ('btn_1' in changed_id) or (n_clicks_b1==0 and n_clicks_b2==0):
-        fig_map = px.choropleth(data_frame=filtered_group_df, #choropleth
+    global button1
+    global button2
+    global filterchange
+    global absrelchange
+    global amount_column
+    if 'Casualty_Severity' in changed_id or 'weather_conditions' in changed_id or 'junction_control' in changed_id:
+        filterchange = True
+    else:
+        filterchange = False
+    if 'btn_1' in changed_id or (n_clicks_b1==0 and n_clicks_b2==0):
+        button1 = True
+        button2 = False
+    elif 'btn_2' in changed_id:
+        button1 = False
+        button2 = True
+    if ('btn_4' in changed_id and (n_clicks_b4 % 2 == 0)) or (n_clicks_b4==0):
+        amount_column = 'Accidents_amount'
+        absrelchange = True
+    elif 'btn_4' in changed_id and (n_clicks_b4 % 2 == 1):
+        amount_column = 'per10000'
+        absrelchange = True
+    else:
+        absrelchange=False
+    if (button1 or (button1 and (filterchange or absrelchange)) or (n_clicks_b1==0 and n_clicks_b2==0)):
+        fig_map = px.choropleth(data_frame=merged, #choropleth map
                             geojson=hucs_rewound, color="Accidents_amount",
                             locations="Local_Authority_(District)", featureidkey="properties.NAME_3",
-                            projection="mercator", range_color=[0, filtered_group_df['Accidents_amount'].max()],
-                            color_continuous_scale=colorscale_list[n_clicks_b3], height=500)
+                            projection="mercator", range_color=[0, merged[amount_column].max()],
+                            color_continuous_scale=colorscale_list[n_clicks_b3%4], height=500)
         fig_map.update_geos(fitbounds="locations", visible=False)
         fig_map.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, coloraxis_colorbar=dict(
             title="Number of accidents",
             len=0.5))  # , title=dict(text="A Figure Specified By A Graph Object"), title_font_size= 30)
-    if 'btn_2' in changed_id:
-        fig_map = px.density_mapbox(filtered_df_junction, lat='Latitude', lon='Longitude', radius=3, #density
-                                center=dict(lat=54.328506, lon=-2.744644), zoom=7,
+    if button2 or (filterchange and (filterchange or absrelchange)):
+        fig_map = px.density_mapbox(filtered_df_junction, lat='Latitude', lon='Longitude', radius=3, #density map
+                                center=dict(lat=51.5085300, lon=-0.1257400), zoom=7,
                                 mapbox_style='mapbox://styles/lucasvm/ckypzzw6kq7i815pcy5ig2slk',
-                                color_continuous_scale=colorscale_list[n_clicks_b3])
+                                color_continuous_scale=colorscale_list[n_clicks_b3%4])
 
     text = 'The level of severity chosen was: {}, weather condition {} and junction control {}.'.format(option_selected,
                                                                                                         option_selected2,
